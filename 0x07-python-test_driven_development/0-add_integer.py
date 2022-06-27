aa3500e7628f265b818867d1dab608cb75f343f8
NatnaelDemelash <natidemelash074@gmail.com>
2022-06-27 00:09:21 -0700

    Test driven development tasks

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+add two intigers
+"""
+def add_integer(a, b=98):
+     """ This function return the sum of intgers
+         or the int cast of floats
+     """  
+     if type(a) != int and type(a) != float:
+         raise TypeError("a must be an integer")
+     if type(b) != int and type(b) != float:
+         raise TypeError("b must be an integer")
+     
+     if type(a) == float:
+         a = int(a)
+     if type(b) == float:
+         b = int(b)
+
+     return a + b

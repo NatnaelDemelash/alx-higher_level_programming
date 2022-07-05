7e6a7f78294cf2d753a12b583c5ef9970069fc2f
NatnaelDemelash <natidemelash074@gmail.com>
2022-07-05 10:52:32 -0700

    updated file

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,49 +1,44 @@
 #!/usr/bin/python3
-"""Documentation for a Student class"""
-
+'''A module for managing students.
+'''
 
 class Student:
-
-    """The Student class"""
-
+    '''Represents a student.
+    '''
     def __init__(self, first_name, last_name, age):
-        """Instatiation function for Student class
-        Args:
-            first_name (str): the first name of the student
-            last_name (str): the last name of the student
-            age (int): the age of the student
-        """
-
+        '''Initializes this student with the given first name,
+        last name, and age.
+        '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Returns the dictionary representation of the object
+        '''Retrieves a dictionary of this student's attributes.
+            
         Args:
-            attrs (list): the attributes to include in the dictionary
+            attrs (list): A list of attributes that can be retrieved.
         Returns:
-            A dictionary representation of the object
-        """
-
-        if not isinstance(attrs, list):
-            return self.__dict__
-        for var in attrs:
-            if not isinstance(var, str):
-                return self.__dict__
-        string_dict = {}
-        for string in attrs:
-            if string in self.__dict__.keys():
-                string_dict[string] = self.__dict__[string]
-        return string_dict
+            dict: A dictionary of this student's attributes.
+        '''
+        if '__dict__' in dir(self):
+            res = dict()
+            can_filter = False
+            if (type(attrs) is list) and all(type(s) is str for s in attrs):
+                can_filter = True
+            for key in self.__dict__.keys():
+                if (not can_filter) or (can_filter and key in attrs):
+                    res[key] = self.__dict__[key]
+            return res
 
     def reload_from_json(self, json):
-        """Replaces the attributes in the instance
+        '''Replaces all attributes of this Student instance with the
+        key-value pairs in the given dictionary.
+        
         Args:
-            json (dict): a dictionary of values to replace
-        """
-
-        keys = list(json.keys())
-        for key in keys:
-            if key in self.__dict__.keys():
-                self.__dict__[key] = json[key] 
+            json (dict): A dictionary of new attributes for this student.
+        '''
+        if isinstance(json, dict) and ('__dict__' in dir(self)):
+            # self.__dict__.clear()
+            for key in json.keys():
+                self.__dict__[key] = json[key]

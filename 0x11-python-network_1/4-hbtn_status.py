42e191688b8021099d466c5125cda5a70afb9cfe
NatnaelDemelash <natidemelash074@gmail.com>
2022-09-23 04:51:38 -0700

    Updated file

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,6 +3,7 @@
 another script that fetches https://intranet.hbtn.io/status
 '''
 import requests
+
 if __name__ == "__main__":
     response = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
